df4f496c0775c82b345a49b50c0ac81b26affee9
Robin Brandt <robin.brandt@med.uni-goettingen.de>
2021-11-09 15:06:29 +0100

    adds form data for view

---
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/formdata/GetBorrowedBookListFormData.java
@@ -0,0 +1,20 @@
+package de.codecentric.psd.worblehat.web.formdata;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+/** This class represent the form data of the return book form. */
+public class GetBorrowedBookListFormData {
+
+  @NotEmpty(message = "{empty.returnAllBookFormData.emailAddress}")
+  @Email(message = "{notvalid.returnAllBookFormData.emailAddress}")
+  private String emailAddress;
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+}
