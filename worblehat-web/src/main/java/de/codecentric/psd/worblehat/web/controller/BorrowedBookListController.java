a2a1cfa5095920ee5398f1bf3ab555989df6a207
Robin Brandt <robin.brandt@med.uni-goettingen.de>
2021-11-09 09:48:41 +0100

    fixes return value in controller

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/BorrowedBookListController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/BorrowedBookListController.java
@@ -2,17 +2,12 @@ package de.codecentric.psd.worblehat.web.controller;
 
 import de.codecentric.psd.worblehat.domain.Book;
 import de.codecentric.psd.worblehat.domain.BookService;
-import de.codecentric.psd.worblehat.web.formdata.ReturnAllBooksFormData;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 /** Controller class for the */
 @Controller
@@ -26,17 +21,19 @@ public class BorrowedBookListController {
     List<Book> books = bookService.findAllBooks();
     List<Book> borrowedBooks = null;
     for (Book book : books) {
-      if(book.getBorrowing().getBorrowerEmailAddress() == email) {
+      if (book.getBorrowing().getBorrowerEmailAddress() == email) {
         borrowedBooks.add(book);
       }
     }
-    Collections.sort(borrowedBooks, new Comparator<Book>() {
-      @Override
-      public int compare(final Book object1, final Book object2) {
-        return object1.getTitle().compareTo(object2.getTitle());
-      }
-    });
-    modelMap.addAttribute("borrowedBookList", borrowedBooks);
-    return "borrowedBookList";
+    Collections.sort(
+        borrowedBooks,
+        new Comparator<Book>() {
+          @Override
+          public int compare(final Book object1, final Book object2) {
+            return object1.getTitle().compareTo(object2.getTitle());
+          }
+        });
+    modelMap.addAttribute("borrowedList", borrowedBooks);
+    return "borrowedList";
   }
 }
